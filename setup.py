71e6781986a284733e5cb9c735666f4e43815e0e
ZonderPit <zonderp@milkymail.org>
2024-02-14 23:23:18 +0100

    WebSockets enabled

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 with open('README.md') as file:
     long_description = file.read()
 
-setup(name='python-domintell',
+setup(name='python-domintell-ws',
       version='0.0.17',
-      url='https://github.com/shamanenas/python-domintell',
+      url='https://github.com/ZonderPit/python-domintell-ws',
       license='MIT',
-      author='Zilvinas Binisevicius',
-      install_requires=["pyserial"],
-      author_email='zilvinas@binis.me',
-      description="Python Library for the Domintell protocol",
+      author='Zilvinas Binisevicius, ZonderPit',
+      install_requires=['pyserial', 'websockets', 'voluptuous'],
+      author_email='zilvinas@binis.me, zonderp@milkymail.org',
+      description='Python Library with WebSockets support for the Domintell protocol',
       long_description=long_description,
-      long_description_content_type="text/markdown",
+      long_description_content_type='text/markdown',
       packages=['domintell', 'domintell.utils', 'domintell.connections', 'domintell.messages', 'domintell.modules'],
       platforms='any',
      )
